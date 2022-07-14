6454c2702ce3a83e494e5a7ed193a47e30b8d4f2
cjhird <charlie.hird@googlemail.com>
2022-07-14 16:53:31 +0100

    first commit

---
+++ Scripts/app.js
@@ -0,0 +1,71 @@
+function init() {
+
+  // Think? - Game Functionality:
+
+  // Invaders need to move side to side and down the screen - automatically.
+  // Invaders need to disappear when shot by the spaceship.
+  // Spaceship needs to move only horizontally at the bottom end of screen
+  // spaceship needs to shoot
+  // Once the spaceship destroys the wave of invaders: game starts again or another wave/level
+  // Record game score - everytime an invader is shot the player receives points.
+  // Invaders will periodically drop bombs on the spaceship - if hit lose one life.
+  // When the player runs out of lives: restart game or go to gameover view.
+
+
+  // Elements
+
+
+  // Variables
+
+  // ? Generate grid cells
+  // ? Game score
+  // ? Player lives
+
+  // Execution
+
+  // ? Movement grid function
+
+  // ? Invader auto movement function
+
+  // ? Spaceship movement function
+
+
+
+  // ? Remove invader function
+
+  // ? Shoot function
+
+  // ? Start game function
+  // player presses spacebar to start game
+
+  // ? Gameover function
+  // game ends when
+
+
+  // Events
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+window.addEventListener('DOMContentLoaded', init)
