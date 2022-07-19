b694605b13a80d6176799c31c096600cc20634a6
cjhird <charlie.hird@googlemail.com>
2022-07-19 19:00:53 +0100

    invaders, spaceship, laser:positioning +  movement

--- app.js
+++ app.js
@@ -30,13 +30,18 @@ function init() {
   const width = 11
   const cellCount = width * width
   const cells = []
+  const audio = document.querySelector('audio')
   let invadersRemoved = []
   let currrentSpaceshipIndex = 115
+  let rightPath = true
+  let direction = 1
+  let invadersAuto
+  let gameScore = 0
   
   // ? Generate grid cells
   for (let i = 0; i < cellCount; i++) {
     const cell = document.createElement('div')
-    cell.innerText = i
+    // cell.innerText = i
     cell.dataset.index = i
     cells.push(cell)
     grid.appendChild(cell)
@@ -46,8 +51,7 @@ function init() {
 
   const invaders = [
     2,3,4,5,6,7,8,
-    13,14,15,16,17,18,19,
-    24,25,26,27,28,29,30
+    13,14,15,16,17,18,19
   ]
 
   // Execution
@@ -59,7 +63,7 @@ function init() {
       if (!invadersRemoved.includes(i)) {
         cells[invaders[i]].classList.add('invader')
       }
-      console.log('INVADER ADD FUNCTION LOOP')
+      // console.log('INVADER ADD FUNCTION LOOP')
     }
   }
   addInvaders()
@@ -68,7 +72,7 @@ function init() {
   function removeInvaders() {
     for (let i = 0; i < invaders.length; i++) {
       cells[invaders[i]].classList.remove('invader')
-      console.log('INVADER REMOVE FUNCTION LOOP')
+      // console.log('INVADER REMOVE FUNCTION LOOP')
     }
   }
 
@@ -105,44 +109,132 @@ function init() {
         if (currrentSpaceshipIndex % width < width - 1) currrentSpaceshipIndex += 1
         break
     }
-    console.log('SPACESHIP HAS MOVED')
+    // console.log('SPACESHIP HAS MOVED')
     cells[currrentSpaceshipIndex].classList.add('spaceship')
   }
   document.addEventListener('keydown', moveSpaceship)
 
 
   // ? Invader auto movement function
-  // remove from current position
-  // if invader reach left or right grid edge then switch direction of all invaiders
-  // on edge move down by one
-  // incr positon by one
+  // Still to do:
   // if spaceship class contains bomb: then remove life from playerLives
-  // if spaceship class contains invader: run gameOver function
-  // ...also when gameover execute clear interval
-  // use set interval to call function after x seconds - to repeat
 
+  function moveInvaders() {
+    const leftEdge = invaders[0] % width === 0
+    const rightEdge = invaders[invaders.length - 1] % width === width - 1
+    removeInvaders()
+    // console.log('INVADERS REMOVED')
+    // GRID EDGE RIGHT: move down plus one left
+    if (rightEdge && rightPath) {
+      for (let i = 0; i < invaders.length; i++) {
+        invaders[i] += width + 1
+        direction = - 1
+        rightPath = false
+      }
+    }
 
+    // GRID EDGE LEFT: move down plus one right
+    if (leftEdge && !rightPath) {
+      for (let i = 0; i < invaders.length; i++) {
+        invaders[i] += width - 1
+        direction = 1
+        rightPath = true
+      }
+    }
+
+    // INCREMENT INVADERS: move by one cell in current direction
+    for (let i = 0; i < invaders.length; i++) {
+      invaders[i] += direction
+    }
+
+    addInvaders()
+
+    // ENDGAME SCENARIOS: gameover or player wins
+
+    // Invaders breach Spaceship
+    if (cells[currrentSpaceshipIndex].classList.contains('invader', 'spaceship')) {
+      // update game view to 'GAMEOVER' screen
+      window.alert('GAMEOVER: spaceship is breached')
+      clearInterval(invadersAuto)
+    }
+
+    // Invaders reach bottom of grid
+    for (let i = 0; i < invaders.length; i++) {
+      if (invaders[i] > (cells.length)) {
+        window.alert('GAMEOVER: invaders reach grid bottom')
+        clearInterval(invadersAuto)
+      }
+    }
+
+    // Invaders destroyed, Spaceship/Player wins
+    if (invadersRemoved.length === invaders.length) {
+      window.alert('PLAYER WINS: all invaders have been destroyed')
+      clearInterval(invadersAuto)
+    }
+  }
+  invadersAuto = setInterval(moveInvaders, 800)
 
   // ? Shoot function
-  // laserId variable
+  function shoot(event) {
+    let laserAuto
+    let currentLaserIndex = currrentSpaceshipIndex
+    function moveLaser() {
+      cells[currentLaserIndex].classList.remove('laser')
+      currentLaserIndex -= width
+      cells[currentLaserIndex].classList.add('laser')
+      
+
+      // when laser collides with invader: remove invader and run explosion
+      if (cells[currentLaserIndex].classList.contains('invader')) {
+        cells[currentLaserIndex].classList.remove('laser')
+        cells[currentLaserIndex].classList.remove('invader')
+        cells[currentLaserIndex].classList.add('explosion')
+        audio.src = 'sounds/invader-destroyed.wav'
+        audio.play()
+
+        setTimeout(() => cells[currentLaserIndex].classList.remove('explosion'), 300)
+        clearInterval(laserAuto)
+
+        // record destroyed invaders
+        const invaderRemoved = invaders.indexOf(currentLaserIndex)
+        invadersRemoved.push(invaderRemoved)
+        // console.log('INVADER HAS BEEN DESTROYED')
+        // console.log(`INVADERS DESTROYED = ${invadersRemoved}`)
+
+        // update gameScore variable
+        gameScore += 100
+        scoreBoard.innerHTML = gameScore
+        // console.log('SCORE HAS BEEN UPDATED')
+      }
+    }
+
+    switch (event.key) {
+      case 'ArrowUp':
+        audio.src = 'sounds/laser.wav'
+        audio.play()
+        laserAuto = setInterval(moveLaser, 100)
+    }
+  }
+
+  document.addEventListener('keydown', shoot)
+    
   // laserIndex variable = spaceshipIndex
   // ? laser movement function inside shoot function
   // remove laser class from current position
   // change laser index value
   // add laser class to current position
-
   // if current laserIndex cell contains invader class
   // then remove laser class
   // also remove invader class
   // add boom class to cell - use set timeout to remove class after 0.3 seconds
-
   // remove destroyed invader from invaders array
   // push invader to invadersRemoved array
   // update gameScore variable - add 10 points
   // update scoreBoard with gameScore use innerHTML
   // console log invadersRemoved
-
   // use set interval to repeat laser movement function - laser moves up grid
+
+
 
   // ? Start game function
   // player presses spacebar to start game
